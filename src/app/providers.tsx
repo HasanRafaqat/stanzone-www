f2cc8f65336c495531abc5153570d91e6713873e
HasanRafaqat <hasanrafaqat555@gmail.com>
2025-01-29 23:58:47 +0500

    comment added

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,9 @@
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  //theme set to light 
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">  
       {children}
     </ThemeProvider>
   );
